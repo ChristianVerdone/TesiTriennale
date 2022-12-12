8e99cb37fb4893e7caf542480ee5fab27cc00c8f
Piodanna <56724273+Piodanna@users.noreply.github.com>
2022-12-12 12:10:53 +0100

    update itemConto

--- code/utilsP.py
+++ code/utilsP.py
@@ -1,6 +1,6 @@
 class ItemConto:
     def __int__(self, codConto, descrConto, dataOp, COD, descrOperazione, numDoc,
-                dataDoc, num, dare , avere, saldo, contropartita):
+                dataDoc, num, importo, saldo, contropartita):
         self.codConto = codConto
         self.descrConto = descrConto
         self.dataOp = dataOp
@@ -9,11 +9,48 @@ class ItemConto:
         self.numDoc = numDoc
         self.dataDoc = dataDoc
         self.num = num
-        self.dare = dare
-        self.avere = avere
+        #self.dare = dare
+        #self.avere = avere
+        self.importo = importo
         self.saldo = saldo
         self.contropartita = contropartita
-        self.importo = None
         self.costiDirAttEconomiche = None
         self.costiDirAttNonEconomiche = None
+
+    #Getter
+    def getCodiceConto(self):
+        return self.codConto
+    def getDescrizioneConto(self):
+        return self.descrConto
+    def getDataOperazione(self):
+        return self.dataOp
+    def getCodice(self):
+        return self.COD
+    def getDescrizioneOperazione(self):
+        return self.descrOperazione
+    def getNumeroDocumento(self):
+        return self.numDoc
+    def getDataDocumento(self):
+        return self.dataDoc
+    def getNumero(self):
+        return self.num
+    def getImporto(self):
+        return self.importo
+    def getSaldo(self):
+        return self.saldo
+    def getContropartita(self):
+        return self.contropartita
+
+
+    """
+    def getAvere(self):
+        return self.avere
+    def getDare(self):
+        return self.dare
+    def getCosti_Diretti_Attività_Economiche(self):
+        return self.costiDirAttEconomiche
+    def getCosti_Diretti_Attività_Non_Economiche(self):
+        return self.costiDirAttNonEconomiche
+    """
+
 
