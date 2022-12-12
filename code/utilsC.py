ed97c95e1495b43c2bbde6014525cf19eb348689
Christian Verdone <christian_ve98@hotmail.it>
2022-12-12 17:26:42 +0100

    fix

--- code/utilsC.py
+++ code/utilsC.py
@@ -1,4 +1,7 @@
+import xlrd
 
+from utilsP import ItemConto
+import datetime
 def splitCel (stringa):
     list = stringa.split()
     codConto = list[2]
@@ -6,3 +9,10 @@ def splitCel (stringa):
     for n in range(3, len(list)):
         desConto = desConto + list[n] + ' '
     return codConto, desConto
+
+def createItemConto(cod, des, row, wb):
+    print(row[0])
+    dataOp=datetime.datetime(*xlrd.xldate_as_tuple(row[0], wb.datemode)).strftime("%d/%m/%Y")
+    print(dataOp)
+    item= ItemConto.__init__(cod,des,dataOp)
+    return item
