7887b75ab9f6f6e8f08e76237b0cb6b2bb606982
Christian Verdone <christian_ve98@hotmail.it>
2022-12-12 11:29:06 +0100

    fix

--- code/readfile.py
+++ code/readfile.py
@@ -3,9 +3,9 @@ import openpyxl
 import xlrd
 import xlsxwriter
 from setuptools import sic
+import datetime
 
 from utilsC import splitCel
-
 
 pd.set_option('display.width', 0)
 #df = pd.read_excel("CerictContiEconomici2021-copia.xlsx", sheet_name=1)
@@ -17,7 +17,6 @@ location = "CerictContiEconomici2021-copia.xls"  #prendo il file
 wb = xlrd.open_workbook(location)   #copio un workbook identico al file
 sheet = wb.sheet_by_index(1)    #prendo il foglio di lavoro che mi interessa lavorare
 #print(sheet.cell_value(0, 0))   #accedo alla prima cella -> suggerimento: fare un iterazione come se fosse una matrice
-
 for n in range(0, sheet.nrows):
     temp = sheet.cell_value(n,0)
     if temp.__class__ == str:
