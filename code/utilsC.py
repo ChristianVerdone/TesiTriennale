abcaf881678237f5fb32faf1d8a66d32d1aa01a6
Christian Verdone <christian_ve98@hotmail.it>
2022-12-14 10:54:19 +0100

    Commenti al codice

--- code/utilsC.py
+++ code/utilsC.py
@@ -3,55 +3,44 @@ import xlrd
 from utilsP import ItemConto
 import datetime
 def splitCel (stringa):
-    list = stringa.split() #Elimino gli spazi dal contenuto della cella "stringa"
-    codConto = list[2] #vi
-    desConto = ''
+    list = stringa.split()  #Elimino gli spazi dal contenuto della cella "stringa"
+    codConto = list[2]  #visto che la cella da dividere ha sempre la stessa costruzione, il codice contro è in 3
+    desConto = ''   #per la descrizione del conto mi basta unire tutte le stringe successive al codice conto
     for n in range(3, len(list)):
         desConto = desConto + list[n] + ' '
     return codConto, desConto
 
 def createItemConto(codC, des, row, wb):
-    dataOp = datetime.datetime(*xlrd.xldate_as_tuple(row[0], wb.datemode)).strftime("%d/%m/%Y")
-    cod = row[1]
-    desOp = row[2]
-    numDoc = row[3]
-    dataDoc = datetime.datetime(*xlrd.xldate_as_tuple(row[4], wb.datemode)).strftime("%d/%m/%Y")
-    numFat = row[5]
-    if not row[6] and not row[7]:
-        importo = 0
+    dataOp = datetime.datetime(*xlrd.xldate_as_tuple(row[0], wb.datemode)).strftime("%d/%m/%Y") #funzione che cattura la data
+    cod = row[1]    #codice operazione
+    desOp = row[2]  #descrizione dell'operazione
+    numDoc = row[3] #numero che indica il documento
+    dataDoc = datetime.datetime(*xlrd.xldate_as_tuple(row[4], wb.datemode)).strftime("%d/%m/%Y")    #data di documentazione
+    numFat = row[5] #numero della fattura
+    if not row[6] and not row[7]:   #per la costruzione dell'importo osservo le colonne "dare" e "avere", importo sarà
+        importo = 0                 #positivo se il float sarà sulla colonna dare altrimenti sarà negativo.
     if row[6]:
         temp = row[6]
-        print(temp)
         if isinstance(temp, float):
             importo = temp
             temp = 0
-            print(importo)
         else:
             temp = row[6].strip().replace('.','').replace(',','.')
             x = str(temp)
-            print('x ',x)
             importo = float(x)
             temp = 0
-            print(importo)
     elif row[7]:
         temp = row[7]
         print(temp)
         if isinstance(temp, float):
             importo = -temp
             temp = 0
-            print(importo)
         else:
             temp = row[7].strip().replace('.','').replace(',','.')
             x = str(temp)
             importo = -float(x)
             temp = 0
-            print(importo)
-    saldo = row[8]
-    contr = row[9]
-    print(codC, ' ', dataOp, ' ', importo)
+    saldo = row[8]  #il saldo del conto per ogni operazione
+    contr = row[9]  #la contropartita dell'operazione
     item = ItemConto(codC, des, dataOp, cod, desOp, numDoc, dataDoc, numFat, importo, saldo, contr)
-    print(item)
-
-    #codC, des, dataOp, cod, desOp, numDoc, dataDoc, numFat, importo, saldo, contr = None
-
     return item
