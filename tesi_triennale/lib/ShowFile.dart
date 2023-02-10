00ef7d5fa51d9c5b3b525bc0ff7818f8cb3bc8f9
Piodanna <56724273+Piodanna@users.noreply.github.com>
2023-02-10 20:01:52 +0100

    Mayor Release 1.1

--- tesi_triennale/lib/ShowFile.dart
+++ tesi_triennale/lib/ShowFile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show SystemUiOverlayStyle, Uint8List;
+import 'package:flutter/services.dart' show SystemUiOverlayStyle;
 
 class ShowFile extends StatelessWidget{
   final List<List<dynamic>> csvData;
@@ -44,7 +44,7 @@ class ShowFile extends StatelessWidget{
                 ),
               )
                   .toList(),
-              rows: csvData!.getRange(1, csvData!.length)
+              rows: csvData.getRange(1, csvData.length)
                   .map(
                     (csvrow) => DataRow(
                   cells: csvrow
